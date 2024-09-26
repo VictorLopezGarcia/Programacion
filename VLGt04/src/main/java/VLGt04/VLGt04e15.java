665bd833054121ba61e7a1544d84824102f1eec7
Víctor López <victlopgar@gmail.com>
2024-09-26 20:27:35 +0200

    26/09/2024

---
+++ VLGt04/src/main/java/VLGt04/VLGt04e15.java
@@ -0,0 +1,19 @@
+package VLGt04;
+
+import java.util.Scanner;
+
+public class VLGt04e15 {
+    public static void main(String[] args){
+        String cadena;
+
+        System.out.println("Introduce tu cadena:");
+        Scanner teclado = new Scanner(System.in);
+        cadena = teclado.nextLine();
+
+        for (int i = 0; i < cadena.length(); i++) {
+            if(i%2!=0){
+                System.out.print("*");
+            }else System.out.print(cadena.charAt(i));
+        }
+    }
+}
